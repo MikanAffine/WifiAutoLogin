f32febb91fafe7fe436ae4913156da9a278a2406
Qinghe <mikanaffine@outlook.com>
2025-04-29 11:58:42 +0800

    feat: init gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,93 @@
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.mikanaffine.wifiautologin"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mikanaffine.wifiautologin"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("keystore.jks"){
+            val prop = Properties()
+            prop.load(project.rootProject.file("/app/keystore/keystore.properties").inputStream())
+
+            keyAlias = "keystore"
+            keyPassword = prop.getProperty("keyPassword")
+            storeFile = file("/keystore/keystore.jks")
+            storePassword = prop.getProperty("storePassword")
+        }
+    }
+
+    buildTypes {
+        val mySignConfig = signingConfigs.getByName("keystore.jks")
+
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = mySignConfig
+        }
+
+        debug {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = mySignConfig
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.work.runtime.ktx)
+
+    implementation(libs.okhttp)
+    implementation(libs.apache.commons.codec)
+    implementation(libs.gson)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
